e7b3eccd65e9a0c3f07d9e79c100414b3c9ef214
Vivek S A <168256157+vikkishre@users.noreply.github.com>
2026-05-26 10:45:32 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    unordered_set<int> s;
+public:
+    bool isHappy(int n) {
+        s.insert(n);
+        int temp=n;
+        
+        while(true)
+        {
+            int res=0;
+            while(temp>0)
+            {
+                int digit=temp%10;
+                res+=digit*digit;
+                temp=temp/10;
+            }
+            temp=res;
+            if(res==1)
+                return true;
+            else if(s.find(res)!=s.end())
+                break;
+            else
+                s.insert(res);
+        }
+        return false;
+    }
+};
