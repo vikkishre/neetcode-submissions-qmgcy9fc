19479e176346ac89899275511f53184f9ac4a8bc
Vivek S A <168256157+vikkishre@users.noreply.github.com>
2026-05-31 12:48:13 +0530

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string res="";
+    int len;
+    void helper(int i,int j,string& s)
+    {
+        len=0;
+        int tempI=i,tempJ=j;
+        if(tempI< 0 || tempJ>=s.length())
+            return;
+        while(tempI>=0 && tempJ<s.length())
+        {
+            if(s[tempI]==s[tempJ])
+            {
+                len=tempJ-tempI+1;
+                if(len>res.length())
+                {
+                    res=s.substr(tempI,tempJ-tempI+1);
+                }
+                tempI--;
+                tempJ++;
+            }
+            else{
+                break;
+            }
+        }
+        helper(i+1,j+1,s);
+    }
+    string longestPalindrome(string s) {
+        helper(0,0,s);
+        helper(0,1,s);
+        return res;
+    }
+};
