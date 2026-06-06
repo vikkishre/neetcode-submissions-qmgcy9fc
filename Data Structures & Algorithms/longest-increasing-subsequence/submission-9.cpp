9d8a9a4daf609be42cdca7255077bdaf8cf8a5f4
Vivek S A <168256157+vikkishre@users.noreply.github.com>
2026-06-06 11:09:57 +0530

    Add: longest-increasing-subsequence - submission-9

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-9.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int helper(int i,int p,vector<int>& nums,vector<vector<int>> &dp)
+    {
+        int n=nums.size();
+        if(i>=n)
+            return 0;
+        if(dp[i][p+1]!=-1)
+            return dp[i][p+1];
+        int res1=helper(i+1,p,nums,dp);
+        if(p==-1 || nums[i]>nums[p])
+        {
+            res1=max(res1,1+helper(i+1,i,nums,dp));
+        }
+        return dp[i][p+1]=res1;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        dp.resize(nums.size()+1,vector<int>(nums.size()+1,-1));
+        return helper(0,-1,nums,dp);
+    }
+};
