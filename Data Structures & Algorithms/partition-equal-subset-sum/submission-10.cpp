a788850ae6e1cb11218781103e0aaa91604f8b90
Vivek S A <168256157+vikkishre@users.noreply.github.com>
2026-06-07 16:20:25 +0530

    Add: partition-equal-subset-sum - submission-10

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-10.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    bool helper(int i,int sum,int target,vector<int>&nums)
+    {
+        if(sum==target)
+            return true;
+        if(i>=nums.size() || sum>target)
+            return false;
+        if(dp[i][sum]!=-1)
+            return dp[i][sum];
+        return dp[i][sum]=helper(i+1,sum+nums[i],target,nums) || helper(i+1,sum,target,nums);
+    }
+    bool canPartition(vector<int>& nums) {
+        
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+        }
+        if(sum%2!=0)
+            return false;
+        dp.resize(nums.size(),vector<int>(sum,-1));
+        return helper(0,0,sum/2,nums);
+    }
+};
